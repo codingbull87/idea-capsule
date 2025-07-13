6b2a0ed646dcbda2b45594fcaea61002866e6dae
Zhe Liu <zheliu@Zhedebijibendiannao.local>
2025-07-13 12:21:25 +0800

    Fix: Debug and resolve local connection issues

--- server/server.js
+++ server/server.js
@@ -6,21 +6,9 @@ const capsuleRoutes = require('./routes/capsuleRoutes');
 
 const app = express();
 
-// --- CORS 配置 ---
-// 创建一个白名单，只允许我们的前端应用访问
-const whitelist = ['http://localhost:3000']; 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) { // !origin 允许 postman 等工具的访问
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
 // --- 中间件 (Middleware) ---
-app.use(cors(corsOptions)); // 使用我们配置好的CORS选项
+// 在开发环境中，我们可以暂时允许所有来源的请求来简化调试
+app.use(cors()); 
 app.use(express.json()); // 解析JSON格式的请求体
 
 // --- 数据库连接 ---
